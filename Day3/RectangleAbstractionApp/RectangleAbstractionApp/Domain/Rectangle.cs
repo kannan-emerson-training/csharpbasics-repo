6ec09c81d0c842e5504c18453611f55deeb3e09d
Kannan <kannans89@hotmail.com>
2025-11-10 21:37:37 +0530

    oop first program

---
+++ Day3/RectangleAbstractionApp/RectangleAbstractionApp/Domain/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace RectangleAbstractionApp.Domain
+{
+    class Rectangle
+    {
+        //attributes 
+        public int width;
+        public int height;
+        public string color;
+
+        public int CalculateArea()
+        {
+
+            return width * height;
+        }
+
+    }
+}
---
+++ Day3/RectangleAbstractionApp/RectangleAbstractionApp/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using RectangleAbstractionApp.Domain;
+
+namespace RectangleAbstractionApp
+{
+     class Program
+    {
+        static void Main()
+        {
+            Rectangle small;//small expects an object of rectnagle
+            small = new Rectangle();
+
+            small.width = 10;
+            small.height = 20;
+            small.color = "red";
+
+            Console.WriteLine($"small width is {small.width}," +
+                $"small height is {small.height},color is {small.color}");
+            Console.WriteLine($"Area of small is {small.CalculateArea()}");
+
+               
+            
+        }
+    }
+}
